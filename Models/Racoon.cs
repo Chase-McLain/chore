85f05e3f6d33f45a20feb4b103af23d74617aa04
Chase McLain <chasermclain@gmail.com>
2025-10-14 14:48:40 -0600

    racoon model

--- Models/Racoon.cs
+++ Models/Racoon.cs
@@ -1,5 +1,17 @@
+namespace racoon_api.Models;
 
 
+
+public class Racoon
+{
+
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public int Age { get; set; }
+  public string color { get; set; }
+  public bool isCool { get; set; }
+
+}
 
 
 
